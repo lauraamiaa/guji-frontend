22555af20be4606196a05c39df0301867c654c70
lauraamiaa <miatkowski.laura@gmail.com>
2021-10-17 02:41:59 +0200

    styled product details page

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -45,7 +45,7 @@ export default function DetailsPage() {
     history.push("/cart");
   }
 
-  if (!coffeeDetails) return <h1>Loading ...</h1>;
+  if (!coffeeDetails) return <h1 className="loading">Loading ...</h1>;
 
   const pricePerBag =
     productOrder.weight && productOrder.weight === "1 KG"
@@ -53,116 +53,130 @@ export default function DetailsPage() {
       : coffeeDetails.price;
 
   return (
-    <div class="detailsPage">
+    <div className="detailsPage">
       {!coffeeDetails ? (
-        "...Loading"
+        ""
       ) : (
         <div>
           <h1 className="coffeeDetailsTitle">{coffeeDetails.name}</h1>
           <div className="description">
-            <p>{coffeeDetails.longDescription}</p>
+            <p className="longDescription">{coffeeDetails.longDescription}</p>
           </div>
+
           <div className="shoppingOptions">
-            <img
-              className="coffeeImage"
-              src={coffeeDetails.imageUrl}
-              alt={coffeeDetails.name}
-            />
-
-            <form>
-              <h3 className="price">PRICE PER BAG</h3>
-              <h3 className="price">€ {pricePerBag}</h3>
-              <h3 className="grind">GRIND SIZE</h3>
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="whole beans"
-                  name="grind"
-                  required="required"
-                />{" "}
-                WHOLE BEANS
-              </label>
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="filter"
-                  name="grind"
-                />{" "}
-                FILTER
-              </label>
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="aeropress"
-                  name="grind"
-                />{" "}
-                AEROPRESS
-              </label>
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="french press"
-                  name="grind"
-                />{" "}
-                FRENCH PRESS
-              </label>
-              <h3 className="size">SIZE</h3>
-
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="250 GR"
-                  name="weight"
-                  required="required"
-                />{" "}
-                250 GR BEANS
-              </label>
-              <label>
-                <input
-                  onChange={(event) => onChangeHandler(event)}
-                  type="radio"
-                  value="1 KG"
-                  name="weight"
-                />{" "}
-                1 KG
-              </label>
-
-              <h3 className="quantity">QUANTITY</h3>
-              <input
-                onChange={(event) => {
-                  onChangeHandler(event);
-                }}
-                type="number"
-                id="quantity"
-                name="quantity"
-                min="1"
-                max="10"
-                required="required"
+            <div>
+              <img
+                className="coffeeImage"
+                src={coffeeDetails.imageUrl}
+                alt={coffeeDetails.name}
               />
-              <h3 className="total">TOTAL AMOUNT</h3>
-              <h3 className="total">
-                €{" "}
-                {!productOrder.quantity
-                  ? 0
-                  : calculateTotalPrice(
-                      productOrder.quantity,
-                      coffeeDetails.price,
-                      productOrder.weight
-                    )}
-              </h3>
-              <button
-                onClick={(event) => onClickHandler(event)}
-                className="button"
-                type="submit"
-              >
-                ADD TO CART
-              </button>
-            </form>
+            </div>
+            <div>
+              <form>
+                <h3 className="price">PRICE PER BAG</h3>
+                <h3 className="pricePerBag">€ {pricePerBag}</h3>
+
+                <h3 className="grind">GRIND SIZE</h3>
+                <div className="grindInputs">
+                  <label className="grindSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="whole beans"
+                      name="grind"
+                      required="required"
+                    />{" "}
+                    WHOLE BEANS
+                  </label>
+                  <label className="grindSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="filter"
+                      name="grind"
+                    />{" "}
+                    FILTER
+                  </label>
+                  <label className="grindSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="aeropress"
+                      name="grind"
+                    />{" "}
+                    AEROPRESS
+                  </label>
+                  <label className="grindSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="french press"
+                      name="grind"
+                    />{" "}
+                    FRENCH PRESS
+                  </label>
+                </div>
+
+                <h3 className="size">SIZE</h3>
+                <div className="sizeInputs">
+                  <label className="sizeSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="250 GR"
+                      name="weight"
+                      required="required"
+                    />{" "}
+                    250 GR
+                  </label>
+                  <label className="sizeSelector">
+                    <input
+                      onChange={(event) => onChangeHandler(event)}
+                      type="radio"
+                      value="1 KG"
+                      name="weight"
+                    />{" "}
+                    1 KG
+                  </label>
+                </div>
+
+                <h3 className="quantity">QUANTITY</h3>
+                <div className="quantityInput">
+                  <input
+                    className="quantitySelector"
+                    onChange={(event) => {
+                      onChangeHandler(event);
+                    }}
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    min="1"
+                    max="10"
+                    required="required"
+                  />
+                </div>
+
+                <h3 className="total">TOTAL AMOUNT</h3>
+                <h3 className="total">
+                  €{" "}
+                  {!productOrder.quantity
+                    ? 0
+                    : calculateTotalPrice(
+                        productOrder.quantity,
+                        coffeeDetails.price,
+                        productOrder.weight
+                      )}
+                </h3>
+
+                <button
+                  onClick={(event) => onClickHandler(event)}
+                  className="addButton"
+                  type="submit"
+                >
+                  ADD TO CART
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       )}
