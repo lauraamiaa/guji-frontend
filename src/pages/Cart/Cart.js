8302d3018d0061d9fde15d742cb1ecf7d3fa893a
lauraamiaa <miatkowski.laura@gmail.com>
2021-10-17 16:36:30 +0200

    styled cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -45,146 +45,209 @@ export default function Cart() {
     <div className="cart">
       <h1 className="cartTitle">YOUR CART</h1>
 
-      {allCartInfo.map((cart, index) => {
-        return (
-          <CartItem
-            key={cart.coffeeId}
-            index={index}
-            coffeeId={cart.coffeeId}
-            price={cart.price}
-            quantity={cart.quantity}
-            weight={cart.weight}
-            grind={cart.grind}
-          />
-        );
-      })}
-
-      {!allCartInfo ? (
-        <div>
-          <h3>YOUR CART IS CURRENTLY EMPTY</h3>
-          <p>
+      {allCartInfo ? (
+        <div className="cartEmpty">
+          <h3 className="cartEmptyTitle">YOUR CART IS CURRENTLY EMPTY</h3>
+          <p className="cartEmptyText">
             Go to the{" "}
             <span>
-              <Link to="/webshop">webshop</Link>
+              <Link className="webshopLink" to="/webshop">
+                webshop
+              </Link>
             </span>{" "}
             to add some products
           </p>
         </div>
       ) : null}
 
-      {allCartInfo ? (
+      <div className="cartDisplay">
         <div>
-          <h3>SUBTOTAL</h3>
-          {/* <p>{subtotal}</p> */}
-          <h4>Shipping Costs € 6.00</h4>
-          <h3>TOTAL</h3>
-          <button
-            onClick={() =>
-              showShippingForm
-                ? setShowShippingForm(false)
-                : setShowShippingForm(true)
-            }
-          >
-            CHECKOUT
-          </button>
+          {allCartInfo.map((cart, index) => {
+            return (
+              <CartItem
+                key={cart.coffeeId}
+                index={index}
+                coffeeId={cart.coffeeId}
+                price={cart.price}
+                quantity={cart.quantity}
+                weight={cart.weight}
+                grind={cart.grind}
+              />
+            );
+          })}
+        </div>
+
+        <div>
+          {allCartInfo ? (
+            <div className="totalCalculation">
+              <h3 className="subTotalTitle">SUBTOTAL</h3>
+              {/* <h3 className="subTotalTitle">{subtotal}</h3> */}
+              <h3 className="subTotalTitle">€ 6.00 - SHIPPING COSTS</h3>
+              <h3 className="subTotalTitle">TOTAL</h3>
+              {/* <h3 className="subTotalTitle">{total}</h3> */}
+              <button
+                className="checkoutButton"
+                onClick={() =>
+                  showShippingForm
+                    ? setShowShippingForm(false)
+                    : setShowShippingForm(true)
+                }
+              >
+                CHECKOUT
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+
+      {showShippingForm && token ? (
+        <div className="shippingForm">
+          <form>
+            <h1 className="shippingTitle">SHIPPING INFORMATION</h1>
+
+            <div>
+              <label className="shippingLabels">
+                FIRST NAME:
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="Enter your first name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+              <label className="shippingLabels">
+                LAST NAME:
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="Enter your surname"
+                    name="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+            </div>
+
+            <div>
+              <label className="shippingLabels">
+                STREET AND HOUSE NUMBER:
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="Street Name 1-3"
+                    name="streetAndHouseNumber"
+                    value={streetAndHouseNumber}
+                    onChange={(e) => setStreetAndHouseNumber(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+            </div>
+            <div>
+              <label className="shippingLabels">
+                APARTMENT, SUITE, ETC. (OPTIONAL):
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="Apartment 4"
+                    name="additionalInfo"
+                    value={additionalInfo}
+                    onChange={(e) => setAdditionalInfo(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+            </div>
+
+            <div>
+              <label className="shippingLabels">
+                POSTAL CODE:
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="1010 DE"
+                    name="postalCode"
+                    value={postalCode}
+                    onChange={(e) => setPostalCode(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+              <label className="shippingLabels">
+                CITY:
+                <div>
+                  <input
+                    className="shippingInputs"
+                    type="text"
+                    placeholder="Amsterdam"
+                    name="city"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                  />
+                </div>
+              </label>
+            </div>
+
+            <div>
+              <label className="shippingLabels">
+                COUNTRY:
+                <div>
+                  <select
+                    className="shippingInputs"
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                    required
+                    name="country"
+                  >
+                    <option value="theNetherlands">The Netherlands</option>
+                    <option value="belgium">Belgium</option>
+                    <option value="luxembourg">Luxembourg</option>
+                    <option value="germany">Germany</option>
+                  </select>
+                </div>
+              </label>
+            </div>
+
+            <button
+              className="orderButton"
+              type="submit"
+              onClick={handleSubmitOrder}
+            >
+              COMPLETE ORDER
+            </button>
+          </form>
         </div>
       ) : null}
 
-      {showShippingForm && token ? (
-        <form>
-          <label>
-            FIRST NAME:
-            <input
-              type="text"
-              name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            LAST NAME:
-            <input
-              type="text"
-              name="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            STREET AND HOUSE NUMBER:
-            <input
-              type="text"
-              name="streetAndHouseNumber"
-              value={streetAndHouseNumber}
-              onChange={(e) => setStreetAndHouseNumber(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            APARTMENT, SUITE, ETC. (OPTIONAL):
-            <input
-              type="text"
-              name="additionalInfo"
-              value={additionalInfo}
-              onChange={(e) => setAdditionalInfo(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            POSTAL CODE:
-            <input
-              type="text"
-              name="postalCode"
-              value={postalCode}
-              onChange={(e) => setPostalCode(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            CITY:
-            <input
-              type="text"
-              name="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            COUNTRY:
-            <select
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              required
-              name="country"
-            >
-              <option value="theNetherlands">The Netherlands</option>
-              <option value="belgium">Belgium</option>
-              <option value="luxembourg">Luxembourg</option>
-              <option value="germany">Germany</option>
-            </select>
-          </label>
-          <button type="submit" onClick={handleSubmitOrder}>
-            COMPLETE ORDER
-          </button>
-        </form>
-      ) : null}
-
       {showShippingForm && !token ? (
-        <div>
-          <p>
+        <div className="notLoggedIn">
+          <p className="notLoggedInText">
             Already have an account?
             <span>
               {" "}
-              <Link to="/login">Log in</Link>
+              <Link className="notLoggedInLink" to="/login">
+                Log in
+              </Link>
             </span>
           </p>
-          <p>
+          <p className="notLoggedInText">
             Don't have one yet, but want to{" "}
             <span>
-              <Link to="/signup">sign up</Link>
+              <Link className="notLoggedInLink" to="/signup">
+                sign up
+              </Link>
             </span>{" "}
             ?
           </p>
