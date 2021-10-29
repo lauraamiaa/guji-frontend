fd15440a477adc3744cf2cb206b44268d24430a6
lauraamiaa <miatkowski.laura@gmail.com>
2021-10-29 12:10:58 +0200

    set up everything for netlify deploy

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,2 @@
-export const apiUrl =
-  process.env.REACT_APP_API_URL ||
-  "postgres://dqvywykdecpdyx:efd420e2de9391894782d637d64846efc59ca45f8e41d9178530f586868f3838@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/df73355q67p1g3";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
